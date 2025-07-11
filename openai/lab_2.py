bd6e4163268ec989dcd9e97ce9d40f4d6f456389
ChetanUpreti99 <chetanupreti97@gmail.com>
2025-07-11 21:27:43 +0530

    feat: lab_2 completed

--- openai/lab_2.py
+++ openai/lab_2.py
@@ -80,7 +80,7 @@ sales_agent3 = Agent(
 @function_tool
 def send_email(body: str):
     """ Send out an email with the given body to all sales prospects """
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    sg = sendgrid.SendGridAPIClient(api_key=sendgrid_api_key)
     from_email = Email("chetanupreti97@gmail.com")  # Change to your verified sender
     to_email = To("sidharthaupreti@gmail.com")  # Change to your recipient
     content = Content("text/plain", body)
@@ -89,7 +89,7 @@ def send_email(body: str):
     return {"status": "success"}
 
 
-print(send_email)
+# print(send_email)
 
 async def main():
     # Run the sales agents in parallel
@@ -116,3 +116,98 @@ async def main():
 
 # asyncio.run(main())
 
+
+
+message = "Send a cold sales email addressed to 'Dear CEO'"
+
+async def main():
+        description = "Write a cold sales email" 
+        tool1=sales_agent1.as_tool(tool_name="sales_agent1", tool_description=description)
+        tool2=sales_agent2.as_tool(tool_name="sales_agent2", tool_description=description)
+        tool3=sales_agent2.as_tool(tool_name="sales_agent3", tool_description=description)
+
+        tools = [tool1, tool2, tool3, send_email]
+
+        instructions ="You are a sales manager working for ComplAI. You use the tools given to you to generate cold sales emails. \
+        You never generate sales emails yourself; you always use the tools. \
+        You try all 3 sales_agent tools once before choosing the best one. \
+        You pick the single best email and use the send_email tool to send the best email (and only the best email) to the user."
+
+
+        sales_manager = Agent(name="Sales Manager", instructions=instructions, tools=tools, model="gpt-4o-mini")
+        message = "Send a cold sales email addressed to 'Dear CEO'"
+        with trace("Sales manager"):
+            result = await Runner.run(sales_manager, message)
+            print(f"Final output:\n{result.final_output}")
+
+# asyncio.run(main())
+
+
+@function_tool
+def send_html_email(subject: str, html_body: str) -> Dict[str, str]:
+    """ Send out an email with the given subject and HTML body to all sales prospects """
+    sg = sendgrid.SendGridAPIClient(api_key=sendgrid_api_key)
+    from_email = Email("chetanupreti97@gmail.com")  # Change to your verified sender
+    to_email = To("sidharthaupreti@gmail.com")  # Change to your recipient
+    content = Content("text/html", html_body)
+    mail = Mail(from_email, to_email, subject, content).get()
+    sg.client.mail.send.post(request_body=mail)
+    return {"status": "success"}
+
+async def main():
+    subject_instructions = "You can write a subject for a cold sales email. \
+    You are given a message and you need to write a subject for an email that is likely to get a response."
+
+    html_instructions = "You can convert a text email body to an HTML email body. \
+    You are given a text email body which might have some markdown \
+    and you need to convert it to an HTML email body with simple, clear, compelling layout and design."
+
+    subject_writer = Agent(name="Email subject writer", instructions=subject_instructions, model="gpt-4o-mini")
+    subject_tool = subject_writer.as_tool(tool_name="subject_writer", tool_description="Write a subject for a cold sales email")
+
+    html_converter = Agent(name="HTML email body converter", instructions=html_instructions, model="gpt-4o-mini")
+    html_tool = html_converter.as_tool(tool_name="html_converter",tool_description="Convert a text email body to an HTML email body")
+
+    tools = [subject_tool, html_tool, send_html_email]
+
+
+    instructions ="You are an email formatter and sender. You receive the body of an email to be sent. \
+                    You first use the subject_writer tool to write a subject for the email, then use the html_converter tool to convert the body to HTML. \
+                    Finally, you use the send_html_email tool to send the email with the subject and HTML body."
+
+    emailer_agent = Agent(
+        name="Email Manager",
+        instructions=instructions,
+        tools=tools,
+        model="gpt-4o-mini",
+        handoff_description="Convert an email to HTML and send it")
+    
+    handoffs = [emailer_agent]
+
+    description = "Write a cold sales email" 
+    tool1=sales_agent1.as_tool(tool_name="sales_agent1", tool_description=description)
+    tool2=sales_agent2.as_tool(tool_name="sales_agent2", tool_description=description)
+    tool3=sales_agent2.as_tool(tool_name="sales_agent3", tool_description=description)
+
+    tools = [tool1, tool2, tool3]
+    sales_manager_instructions = "You are a sales manager working for ComplAI. You use the tools given to you to generate cold sales emails. \
+                                    You never generate sales emails yourself; you always use the tools. \
+                                    You try all 3 sales agent tools at least once before choosing the best one. \
+                                    You can use the tools multiple times if you're not satisfied with the results from the first try. \
+                                    You select the single best email using your own judgement of which email will be most effective. \
+                                    After picking the email, you handoff to the Email Manager agent to format and send the email."
+    
+    sales_manager = Agent(
+    name="Sales Manager",
+    instructions=sales_manager_instructions,
+    tools=tools,
+    handoffs=handoffs,
+    model="gpt-4o-mini")
+
+    message = "Send out a cold sales email addressed to Dear CEO from Alice"
+
+    with trace("Automated SDR"):
+        result = await Runner.run(sales_manager, message)
+        print(f"Final output:\n{result.final_output}")
+
+# asyncio.run(main())
